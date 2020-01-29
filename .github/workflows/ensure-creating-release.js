fdc067c7944fea1127f0d46169e364d0fffb2158
Sutou Kouhei <kou@clear-code.com>
2020-01-29 14:56:17 +0900

    Use if

--- .github/workflows/ensure-creating-release.js
+++ .github/workflows/ensure-creating-release.js
@@ -1,5 +1,4 @@
 const {owner: owner, repo: repo} = context.repo;
-console.log(context);
 const tag = context.ref.replace("refs/tags/", "")
 try {
   const {data: release} = await github.repos.getReleaseByTag({
